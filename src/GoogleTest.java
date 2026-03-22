bb6863ec4828980a5801d0ae61900c2dd7bb8067
yuvikag <yuvikapgupta@gmail.com>
2026-03-22 17:50:14 +0000

    Initial commit

---
+++ src/GoogleTest.java
@@ -0,0 +1,38 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class GoogleTest {
+    public static void main(String[] args) {
+
+        WebDriver driver = new ChromeDriver();
+
+        // Open Google
+        driver.get("https://www.google.com");
+
+        // Maximize window
+        driver.manage().window().maximize();
+
+        // Find search box and enter text
+        WebElement searchBox = driver.findElement(By.name("q"));
+        searchBox.sendKeys("DevOps Selenium");
+
+        // Submit search
+        searchBox.submit();
+
+        // Wait (just for demo)
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        
+        String title = driver.getTitle();
+        System.out.println("Page Title: " + title);
+        System.out.println("Title Test Passed");
+        // Close browser
+        driver.quit();
+    }
+}

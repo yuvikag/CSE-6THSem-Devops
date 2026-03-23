b7ebf5f8641fce2cf007311dc98223848e62675f
yuvikag <yuvikapgupta@gmail.com>
2026-03-23 15:08:37 +0530

    Refactor GoogleTest.java for cleaner code

--- src/GoogleTest.java
+++ src/GoogleTest.java
@@ -7,31 +7,17 @@ public class GoogleTest {
     public static void main(String[] args) {
 
         WebDriver driver = new ChromeDriver();
-
-        // Open Google
         driver.get("https://www.google.com");
-
-        // Maximize window
         driver.manage().window().maximize();
-
-        // Find search box and enter text
         WebElement searchBox = driver.findElement(By.name("q"));
         searchBox.sendKeys("DevOps Selenium");
-
-        // Submit search
         searchBox.submit();
-
-        // Wait (just for demo)
         try {
             Thread.sleep(3000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-
-        
         String title = driver.getTitle();
-        
-        // Close browser
         driver.quit();
     }
 }
